36e5c114b002e2b542127761566d8939498daeb2
renfengz <renfengz@usc.edu>
2019-08-21 11:32:02 -0700

    Updates

---
+++ precache-manifest.70040030d86e900b2a16c26b216621ca.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76011d923f6743b76433b2ee51e366d",
+    "url": "/tryTuka/index.html"
+  },
+  {
+    "revision": "3748df6e0cbce6019f04",
+    "url": "/tryTuka/static/css/2.fe066219.chunk.css"
+  },
+  {
+    "revision": "854c545276f9402911ae",
+    "url": "/tryTuka/static/css/main.3685bd09.chunk.css"
+  },
+  {
+    "revision": "3748df6e0cbce6019f04",
+    "url": "/tryTuka/static/js/2.6e6b174f.chunk.js"
+  },
+  {
+    "revision": "854c545276f9402911ae",
+    "url": "/tryTuka/static/js/main.f7d56b7b.chunk.js"
+  },
+  {
+    "revision": "6c94cd80472c0e7145ea",
+    "url": "/tryTuka/static/js/runtime~main.6a32cab0.js"
+  },
+  {
+    "revision": "c1c05924d7e909a78ea4356635c0c961",
+    "url": "/tryTuka/static/media/IMG_3138.c1c05924.jpg"
+  },
+  {
+    "revision": "a2d550c75b452307c7991ec5b6b75b62",
+    "url": "/tryTuka/static/media/artist1.a2d550c7.png"
+  },
+  {
+    "revision": "d3702575185ff49e1ad921d9f609acc7",
+    "url": "/tryTuka/static/media/artist2.d3702575.png"
+  },
+  {
+    "revision": "4d8d839e03fbb29737e29fcd71c2c417",
+    "url": "/tryTuka/static/media/artist3.4d8d839e.png"
+  },
+  {
+    "revision": "eb54f39c23ef6d80ed9b2c1cd56442ad",
+    "url": "/tryTuka/static/media/artist4.eb54f39c.png"
+  },
+  {
+    "revision": "783a26830de542d95d132fa8e7119b4b",
+    "url": "/tryTuka/static/media/artist5.783a2683.png"
+  },
+  {
+    "revision": "326306f2249d5bd03faedbbafb64e7fc",
+    "url": "/tryTuka/static/media/artist6.326306f2.png"
+  },
+  {
+    "revision": "689dc886317a537df255fbc2bb0169c5",
+    "url": "/tryTuka/static/media/icon-1.689dc886.png"
+  },
+  {
+    "revision": "fafeac361f112cbcf9913cbdcee43bf4",
+    "url": "/tryTuka/static/media/icon-2.fafeac36.png"
+  },
+  {
+    "revision": "655ef7857c927e156a713485af7c2bd6",
+    "url": "/tryTuka/static/media/icon-3.655ef785.png"
+  },
+  {
+    "revision": "6520cafbec94cdc6bc6917809199a491",
+    "url": "/tryTuka/static/media/icon-4.6520cafb.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/tryTuka/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/tryTuka/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/tryTuka/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/tryTuka/static/media/slick.f97e3bbf.svg"
+  }
+]);
